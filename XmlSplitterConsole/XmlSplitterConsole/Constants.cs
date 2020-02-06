7f214dcedf55f80792596a3bd8abae6f0bb9d04a
Mohan421 <sinwal92@gmail.com>
2020-02-06 01:41:55 -0500

    Add files via upload

---
+++ XmlSplitterConsole/XmlSplitterConsole/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XmlSplitterConsole
+{
+    public static class Constants
+    {
+        public const string defaultFileName= @"XML\Records.xml";
+        public const string Worker = "Worker";
+        public const string Summary= "Summary";
+        public const string Workers_Effective_Stack = "Workers_Effective_Stack";
+        public const string TagPrefix =  "peci";
+    }
+}
---
+++ XmlSplitterConsole/XmlSplitterConsole/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace XmlSplitterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Application Started!!");
+            Console.WriteLine("Processing File!!");
+       
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,Constants.defaultFileName);
+            string fileName = Path.GetFileNameWithoutExtension(path);
+            XDocument xdoc = XDocument.Load(path);
+            XNamespace tagPrefix = xdoc.Root.Name.Namespace;
+            int count = xdoc.Descendants(tagPrefix + Constants.Worker).Count();
+            var summaryNode = xdoc.Descendants(tagPrefix + Constants.Summary).FirstOrDefault();
+            int skip = 0;
+            int take = 10;
+            int fileNo = 0;
+
+            // Split elements into chunks and save to disk.
+            while (skip < count)
+            {
+                // Extract portion of the xml elements.
+                var workerNodes = xdoc.Descendants(tagPrefix + Constants.Worker)
+                            .Skip(skip)
+                            .Take(take);
+                // Setup number of elements to skip on next iteration.
+                skip += take;
+                // File sequence no for split file.
+                fileNo += 1;
+                // Filename for split file.
+                var outputFilename = String.Format("{0}" + "_{1}.xml", fileName, fileNo);
+                // Create a partial xml document.
+                XElement workerEffectiveStack = new XElement(tagPrefix+Constants.Workers_Effective_Stack, workerNodes);
+                // Save to disk.
+                XElement xmlElements = new XElement(tagPrefix + Constants.Workers_Effective_Stack, new XAttribute(XNamespace.Xmlns + Constants.TagPrefix, tagPrefix.NamespaceName), summaryNode, workerNodes);                
+                xmlElements.Save(Path.Combine(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"XmlOutput"), outputFilename));
+            }
+            Console.WriteLine("Processing Completed!!");
+            Console.ReadKey();
+        }
+    }
+}
